d098d634124d90a5956216f6f8b7157838ede78c
juny0 <juny0@vt.edu>
2018-07-09 20:09:19 -0500

    updating

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ sqlite_file = 'my_db.sqlite' # name of the sqlite database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-# Creating a new SQLite table with 1 column
-c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn='Ingredients', nf='ingredient_name', ft='STRING'))
+c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Ingredients'")
+all_rows = c.fetchall()
+print(all_rows)
 
 # Creating a new SQLite table with 1 column
 c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-        .format(tn='Recipes', nf='recipe_name', ft='STRING'))
+        .format(tn='Ingredients', nf='ingredient_name', ft='TEXT'))
+
+# Creating a new SQLite table with 1 column
+c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+        .format(tn='Recipes', nf='recipe_name', ft='TEXT'))
 
 conn.commit()
+conn.close()
