9e97b3a8aed362556859526abc43c5d70d71697a
juny0 <juny0@vt.edu>
2018-07-09 19:59:30 -0500

    added two tables

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import sqlite3
 sqlite_file = 'my_db.sqlite' # name of the sqlite database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
+
+# Creating a new SQLite table with 1 column
+c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+        .format(tn='Ingredients', nf='ingredient_name', ft='STRING'))
+
+# Creating a new SQLite table with 1 column
+c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+        .format(tn='Recipes', nf='recipe_name', ft='STRING'))
+
+conn.commit()
