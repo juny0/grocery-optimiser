c7fe7da6ed4122eff685eabfd0fc6c15e3b3304b
juny0 <juny0@vt.edu>
2018-07-11 09:46:22 -0500

    renamed database file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-sqlite_file = 'my_db.sqlite' # name of the sqlite database file
+sqlite_file = 'recipes_db_file.sqlite' # name of the sqlite database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
@@ -10,6 +10,7 @@ all_rows = c.fetchall()
 print(all_rows)
 
 if len(all_rows) == 0:
+    print("== Database file does not exist, creating new database file ==")
     # Creating a new SQLite table with 1 column
     c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
             .format(tn='Ingredients', nf='ingredient_name', ft='TEXT'))
