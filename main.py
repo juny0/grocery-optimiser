831b0a9ad710f54f703443a9abbea3d34c9c485f
juny0 <juny0@vt.edu>
2018-07-11 10:34:32 -0500

    added sqlite3 shell for mac

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Check to see if tables have been created already or not
-c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Ingredients'")
-all_rows = c.fetchall()
-print(all_rows)
+# Creating the SQLite table 'Ingredients'
+c.execute('CREATE TABLE IF NOT EXISTS ingredients (ingredientName TEXT PRIMARY KEY)')
 
-if len(all_rows) == 0:
-    print("== Database file does not exist, creating new database file ==")
-    # Creating a new SQLite table with 1 column
-    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-            .format(tn='Ingredients', nf='ingredient_name', ft='TEXT'))
+# Creating the SQLite table 'Recipes'
+c.execute('CREATE TABLE IF NOT EXISTS recipes (recipeName TEXT PRIMARY KEY)')
 
-    # Creating a new SQLite table with 1 column
-    c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
-            .format(tn='Recipes', nf='recipe_name', ft='TEXT'))
+# Creating the SQLite table 'Recipe_Contains'
+create_table_query = (
+    "CREATE TABLE IF NOT EXISTS recipe_contains ( "
+    "ingredientName TEXT, "
+    "recipeName TEXT, "
+    "PRIMARY KEY (ingredientName, recipeName), "
+    "   FOREIGN KEY (ingredientName) REFERENCES ingredients(ingredientName), "
+    "   FOREIGN KEY (recipeName) REFERENCES recipes(recipeName)"
+    ")"
+    )
+c.execute(create_table_query)
 
 conn.commit()
 conn.close()
